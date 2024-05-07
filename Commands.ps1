b0a6ef53ceaf9e154fbdc964a7be8bd67b8ac873
HABİB MÜKÜS <habib.mukus@turkiyehayatemeklilik.com.tr>
2024-05-07 17:12:58 +0300

    new commands added

--- Commands.ps1
+++
@@ -1,8 +0,0 @@
-Lock Screen (Win + L)
-rundll32.exe user32.dll,LockWorkStation
-
-print svchost.exe with pid
-tasklist /SVC /FI "IMAGENAME eq svchost.exe"
-
-ADUser Properties
-Get-ADUser -Identity "USERNAME" -Properties *
--- hostname.ps1
+++ hostname.ps1
@@ -5,4 +5,4 @@ $middleIndex = [math]::floor($lastFiveCharacters.Length / 2)
 $modifiedString = $lastFiveCharacters.Substring(0, $middleIndex) + $lastFiveCharacters.Substring($middleIndex + 1)
 $result = "t00" + $modifiedString
 
-Remove-Item -Force -Recurse "C:\Users\$result\AppData\Local\Google\Chrome\User Data\Default\Extensions\dehobbhellcfbmcaeppgfjhnldeimdph"
+Remove-Item -Force -Recurse "C:\Users\$result\AppData\Local\Google\Chrome\User Data\Default\Extensions\abcdef...."
