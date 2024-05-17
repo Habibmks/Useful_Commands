b2fe79ab2d32a895d9dbc6fae4548b27812131c9
Habib Müküs <43863662+Habibmks@users.noreply.github.com>
2024-05-17 10:32:19 +0300

    Update and rename hostname.ps1 to hostname to username.ps1

--- hostname to username.ps1
+++ hostname to username.ps1
@@ -3,6 +3,6 @@ $hostname = hostname
 $lastFiveCharacters = $hostname.Substring($hostname.Length - 5)
 $middleIndex = [math]::floor($lastFiveCharacters.Length / 2)
 $modifiedString = $lastFiveCharacters.Substring(0, $middleIndex) + $lastFiveCharacters.Substring($middleIndex + 1)
-$result = "t00" + $modifiedString
+$result = "username string" + $modifiedString
 
 Remove-Item -Force -Recurse "C:\Users\$result\AppData\Local\Google\Chrome\User Data\Default\Extensions\abcdef...."
