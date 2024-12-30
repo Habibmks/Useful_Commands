1f39a0d4edcc43878ba06bb48196444e223f5c62
HABİB MÜKÜS <habib.mukus@turkiyehayatemeklilik.com.tr>
2024-12-30 12:14:19 +0300

    Edited

--- hostname to username.ps1
+++ hostname to username.ps1
@@ -4,5 +4,3 @@ $lastFiveCharacters = $hostname.Substring($hostname.Length - 5)
 $middleIndex = [math]::floor($lastFiveCharacters.Length / 2)
 $modifiedString = $lastFiveCharacters.Substring(0, $middleIndex) + $lastFiveCharacters.Substring($middleIndex + 1)
 $result = "username string" + $modifiedString
-
-Remove-Item -Force -Recurse "C:\Users\$result\AppData\Local\Google\Chrome\User Data\Default\Extensions\abcdef...."
